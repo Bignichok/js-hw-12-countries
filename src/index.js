2a6f5ddff5caa93ffcf7a42990bdef16df064fb4
Bignichok <bignik545@gmail.com>
2020-09-05 14:51:52 +0300

    some fixes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import './styles.css';
+import '@pnotify/core/dist/BrightTheme.css';
 import renderMarkup from './js/renderMarkup';
 import fetchCountries from './js/fetchCountries';
-import { alert, error, Stack } from '@pnotify/core';
-import '@pnotify/core/dist/BrightTheme.css';
+
+import { debounce } from 'lodash';
+
+import { error, defaultModules } from '@pnotify/core';
+import * as PNotifyMobile from '@pnotify/mobile';
+defaultModules.set(PNotifyMobile, {});
 
 const searchInput = document.querySelector('.search-input');
 const countriesList = document.querySelector('.countries-list');
 const countryWrp = document.querySelector('.country-wrp');
-const debounce = require('lodash.debounce');
 
 searchInput.addEventListener('input', debounce(searchInputHandler, 500));
 
 function searchInputHandler(e) {
-  const target = e.target;
-  const query = target.value;
+  const query = e.target.value;
   countriesList.innerHTML = ' ';
   countryWrp.innerHTML = ' ';
   if (query.length > 1) {
@@ -22,30 +25,9 @@ function searchInputHandler(e) {
         if (data.length > 0 && data.length <= 10) {
           renderMarkup(data);
         } else if (data.length > 10) {
+          error('Too many matches found. Please enter a more specific query!');
         }
       })
       .catch(err => console.log(err));
   }
 }
-
-const myStack = new Stack({
-  dir1: 'down',
-  dir2: 'left',
-  firstpos1: 90,
-  firstpos2: 90,
-});
-
-error({
-  text: 'Too many matches found. Please enter a more specific query!',
-  stack: myStack,
-});
-
-alert({
-  text: "Notice that's positioned in its own stack.",
-  stack: new Stack({
-    dir1: 'down',
-    dir2: 'right', // Position from the top left corner.
-    firstpos1: 90,
-    firstpos2: 90, // 90px from the top, 90px from the left.
-  }),
-});
